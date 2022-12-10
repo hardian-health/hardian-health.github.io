f4de81068b3e9faed2e2815fdc3aa327b107d087
Code + Tonic <90833903+CodeAndTonicHQ@users.noreply.github.com>
2022-12-10 16:39:41 +0200

    read more to portfolio WIP

--- header-development.js
+++ header-development.js
@@ -1,1 +1,19 @@
 // DEVELOPMENT STUFF. ALWAYS KEEP THIS
+
+
+//rewrite the function to add the read more thing as last item in .portfolio-text. we dont need the div, just the a tag but make sure to add the class read-more to the a tag
+function addReadMoreToPortfolioItems() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => {
+    const href = item.getAttribute('href');
+    const portfolioText = item.querySelector('.portfolio-text');
+    portfolioText.insertAdjacentHTML('beforeend', `<a class="js-portfolio-item-read-more" href="${href}">Read More →</a>`);
+    });
+}
+
+//on dom ready call the function
+document.addEventListener('DOMContentLoaded', function() {
+    addReadMoreToPortfolioItems();
+});
+
+   
