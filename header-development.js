f004d063992a48baf822d0ee1c1d02edc2cac875
Code + Tonic <90833903+CodeAndTonicHQ@users.noreply.github.com>
2022-12-10 10:56:47 +0200

    cleaned up pofo seo desctiption addition

--- header-development.js
+++ header-development.js
@@ -5,10 +5,11 @@
 
 
 
-/*
-TUBE MAP DESCRIPTIONS
-
-*/
+/*******
+* ****************
+* TUBE MAP DESCRIPTIONS
+* ****************
+*******/
 
 
 // 1. FUNCTION TO LOOP tube map links and CREATE an array of descriptions in the console
@@ -77,7 +78,7 @@ const descriptions = [
 ]
 
 
-// 3. FUNCTION TO ADD DESCRIPTIONS TO THE TUBE MAP LINKS
+// 3. FUNCTION TO ADD DESCRIPTIONS from the array TO THE TUBE MAP LINKS
 function addDescriptions() {
     //get all the links
     var links = document.querySelectorAll(".grid-item");
@@ -108,9 +109,9 @@ function addDescriptions() {
 
 //add descriptions to the tube map links on document ready
 document.addEventListener("DOMContentLoaded", function(event) {
-    //addDescriptions();
+    //addDescriptions(); // we have a better way to do this now
 });
-addDescriptions();
+
 
 
 
@@ -134,79 +135,80 @@ function fetchAndAddDescriptions(url){
     //create a new fetcUrl by combining current window.location full url with "?format=json-pretty"
     var fetchUrl = window.location.href + "?format=json-pretty";
 
-
-
-
     //fetch cache stuff - "headers"
     var myHeaders = new Headers();
 
     //fetch headers – shouold they be A) cached for speed or B) non-cached for admin to see chances in real time?
+       // Caching the portfolio SEO description for visitors
+    // The portfolio SEO description is not cached for admin users, so we'll only cache it for visitors
+
+    // If the url contains .squarespace.com, we're assuming it's admin logged in and won't cache the portfolio SEO description
+    // If the url does not contain .squarespace.com, we're assuming it's a visitor and will cache the portfolio SEO description
     if(document.location.host.includes(".squarespace.com")){
-    console.log("url contains .squarespace.com so we're assuming it's admin logged in and won't cache the portfolio SEO description-add -fetch");
-    myHeaders.append('pragma', 'no-cache');
-    myHeaders.append('cache-control', 'no-cache');
+        console.log("url contains .squarespace.com so we're assuming it's admin logged in and won't cache the portfolio SEO description");
+        myHeaders.append('pragma', 'no-cache');
+        myHeaders.append('cache-control', 'no-cache');
 
     } else{
-    console.log("url  DOES NOT contain .squarespace.com. Assuming its' a visitor and will cache portfolio SEO description-add -fetch to make everything quicker");
+        console.log("url  DOES NOT contain .squarespace.com. Assuming its' a visitor and will cache portfolio SEO description to make everything quicker");
     }
-
 
 
 
     fetch(fetchUrl, { 
         method: 'GET',
         headers: myHeaders
-      })
-      .then(function(response) { return response.json(); })
-      .then(function(json) {
+    })
+    .then(function(response) { return response.json(); })
+    .then(function(json) {
         // use the json
-          portfolioItems = json.items;
-    
+    portfolioItems = json.items;
 
-          function addSeoDescriptionToPortfolioItems() {
-    portfolioItems.forEach((item) => {
-    if (item.seoData && item.seoData.seoDescription) {
-      const gridItems = document.querySelectorAll(`a.grid-item`);
-      gridItems.forEach((gridItem) => {
-        const href = gridItem.getAttribute('href');
-        if (href === item.fullUrl) {
-          const description = document.createElement('p');
-          description.classList.add('description');
-          description.innerText = item.seoData.seoDescription;
-          const portfolioText = gridItem.querySelector('.portfolio-text');
-          portfolioText.appendChild(description);
+        // This function is called after the portfolio items are loaded
+        // We loop through each JSONitem and check if it has a SEO description
+        // If the SQSP JSON item has a SEO description, we add it to the corresponding a.grid-item
+        function addSeoDescriptionToPortfolioItems() {
+            // Loop through all portfolio items in the JSON
+            portfolioItems.forEach((item) => {
+                // Check if the porftolio item has a SEO description
+                if (item.seoData && item.seoData.seoDescription) {
+                    // Get all grid items (portfolio links in the tube map)
+                    const gridItems = document.querySelectorAll(`a.grid-item`);
+                    // Loop through all grid items
+                    gridItems.forEach((gridItem) => {
+                        // Get the href attribute of the grid item
+                        const href = gridItem.getAttribute('href');
+                        // Check if the link destiontion is the same as the JSON full URL of the portfolio item
+                        if (href === item.fullUrl) {
+                            // Create a paragraph element
+                            const description = document.createElement('p');
+                            // Add a class to the paragraph element
+                            description.classList.add('seo-description');
+                            // Set the inner text of the paragraph element
+                            description.innerText = item.seoData.seoDescription;
+                            // Get the portfolio text element
+                            const portfolioText = gridItem.querySelector('.portfolio-text');
+                            // Append the description to the portfolio text (so it appears below the h3 title)
+                            portfolioText.appendChild(description);
+                        }
+                    });
+                }
+            });
         }
-      });
-    }
-  });
-}
-    
-    
-addSeoDescriptionToPortfolioItems();
-    
-    
-          
+        // Call the function to add the SEO descriptions to the portfolio items
+        addSeoDescriptionToPortfolioItems();
 
     
-     //close the function
-        });
-    // end of function
     
+    });
+} // end of fetchAndAddDescriptions() function
 
 
-}
-
-
-
-
-//end of document ready
-
-
+// check if we are on a portfolio page. if we do, run the function to add SEO descriptions
 if (document.querySelector("#gridThumbs.portfolio-grid-basic")) {
     console.log("we are on a portfolio page");
     fetchAndAddDescriptions();
 }
-
 
 
 //run fetchAndAddDescriptions() on document ready
@@ -221,6 +223,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 //end of document ready
 
 
-//🟢🟢🟢🟢🟢
+// 🟢🟢🟢🟢🟢
 
 
